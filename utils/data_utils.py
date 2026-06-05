803606b8f14f44ac149e71adb99f7cfac8c100db
bhaskar9221 <bhaskarmondal7221@gmail.com>
2026-06-05 16:50:13 +0530

    updating data_utils.py

--- utils/data_utils.py
+++ utils/data_utils.py
@@ -1,8 +1,10 @@
 """
 Splitting the Data into Train and Test Sets for Machine Learning Models
 """
-import numpy as np
+
+import numpy as np 
 from sklearn.datasets import load_iris
+
 def train_test_split(X, y, test_size=0.2, random_state=None):
     """Split data into train and test sets
        Splitting the Iris Dataset(for this demonstration) into training and test sets.)
@@ -11,7 +13,14 @@ def train_test_split(X, y, test_size=0.2, random_state=None):
     if random_state is not None:
         np.random.seed(random_state)
 
-        n = X.shape[0]
-        indices = np.arrange(n)
-        np.random.shuffle(indices)
-        
+    n = X.shape[0]
+
+    indices = np.arange(n)
+    np.random.shuffle(indices)
+    split = int(n * (1 - test_size))
+
+    train_idx = indices[:split]
+    test_idx = indices[split:]
+
+    return X[train_idx], y[train_idx], X[test_idx], y[test_idx]
+
