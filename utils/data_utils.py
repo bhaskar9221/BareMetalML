d43b40088fa198cafc46a9a50e847d16bc9b0dcb
bhaskar9221 <bhaskarmondal7221@gmail.com>
2026-06-05 17:08:17 +0530

    completed and test data_utils.py

--- utils/data_utils.py
+++ utils/data_utils.py
@@ -10,17 +10,22 @@ def train_test_split(X, y, test_size=0.2, random_state=None):
        Splitting the Iris Dataset(for this demonstration) into training and test sets.)
     
     """
-    if random_state is not None:
+
+    #
+    if random_state is not None:              
         np.random.seed(random_state)
 
+    #Getting the row size from the Dataset
     n = X.shape[0]
 
+    #Generating shuffeled indices and determining the train test split point.
     indices = np.arange(n)
     np.random.shuffle(indices)
     split = int(n * (1 - test_size))
 
+    #Initializing the Features and Target Variables for the Train and Test Sets
     train_idx = indices[:split]
     test_idx = indices[split:]
 
+    #Returning the final train and test sets
     return X[train_idx], y[train_idx], X[test_idx], y[test_idx]
-
