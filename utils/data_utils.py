4922400c7e57b4dc846f563ce11322970ad3b895
bhaskar9221 <bhaskarmondal7221@gmail.com>
2026-06-05 16:08:46 +0530

    testing

--- utils/data_utils.py
+++ utils/data_utils.py
@@ -1,3 +1,7 @@
 """
-Splitting Data 
+Splitting the Data into Train and Test Sets for Machine Learning Models
 """
+import numpy as np
+
+def train_test_split(X, y, test_size=0.2, random_state=None):
+    """Split data into train and test sets"""
