4b10b7134b6989075c0df1b93ed9d21cdd98af3d
bhaskar9221 <bhaskarmondal7221@gmail.com>
2026-06-10 11:55:24 +0530

    feat(decision-trees): implemented entropy function

--- notebooks/03_decision_tree/decision_tree.py
+++ notebooks/03_decision_tree/decision_tree.py
@@ -6,3 +6,11 @@ def gini_impurity(y):
     probabilities = counts / len(y)
     gini = 1 - np.sum(probabilities ** 2)
     return gini
+
+def entropy(y):
+    
+    counts = np.bincount(y)
+    probabilities = counts / len(y)
+    probabilities = probabilities[probabilities > 0]
+    entropy = -np.sum(probabilities * np.log2(probabilities))  
+    return entropy
